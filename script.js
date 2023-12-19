861017ba0d175f91185231e762675a8d46310270
OSHAN JR <79368912+0shanjr@users.noreply.github.com>
2023-12-19 20:17:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function changeView(){
+    var signUpBox = document.getElementById("signUpBox");
+    var signInBox = document.getElementById("signInBox");
+
+    signUpBox.classList.toggle("d-none");
+    signInBox.classList.toggle("d-none");
+}
+
+function signUp(){
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var mobile = document.getElementById("mobile");
+    var gender = document.getElementById("gender");
+
+    var form = new FormData();
+    form.append("f",fname.value);
+    form.append("l",lname.value);
+    form.append("e",email.value);
+    form.append("p",password.value);
+    form.append("m",mobile.value);
+    form.append("g",gender.value);
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        if(request.status == 200 & request.readyState == 4){
+            var response = request.responseText;
+            alert(response);
+        }
+    }
+
+    request.open("POST","signUpProcess.php",true);
+    request.send();
+}
